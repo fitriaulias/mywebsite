d1ceb345ef45ccf465595ce86e85b3e5d9e7a9b6
fitriaulias <fitriauliasanni@gmail.com>
2022-01-14 22:26:50 +0700

    Pushed to Github

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function clickHome () {
+    alert("Ini Halaman Home");
+};
+function clickCourse () {
+    alert("Ini Halaman Course");
+};
+function clickBlog () {
+    alert("Ini Halaman Blog");
+};    
+function clickAbout () {
+    alert("Ini Halaman About");
+};   
